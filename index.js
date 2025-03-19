0981ca6c9887b0e658db4b598d78f7e2aa5bd646
Mj Mausam <89700267+mjmausam1012@users.noreply.github.com>
2025-03-19 19:06:04 +0530

    index.js

--- index.js
+++ index.js
@@ -43,8 +43,7 @@ btn_All_thread.addEventListener("click", () => {
 })
 
 const getFacts = async () => {
-    // const URL = "https://hindi-jokes-api.onrender.com/jokes/6?api_key=90012273eeff539c40c4ff3d112d";
-    const URL = "https://quote-generator-api-six.vercel.app/api/quotes/?limit=6";
+    const URL = "https://hindi-jokes-api.onrender.com/jokes/6?api_key=90012273eeff539c40c4ff3d112d";
     let response = await fetch(URL);
     let fact = await response.json();
     fac1.innerText = fact.data[0].jokeContent;;
